794c12056652222694b7945650bef15975b25151
Jegathesan Shanmugam <jega@torqueagi.com>
2025-10-08 16:05:18 +0530

    enable sam2 version download

--- checkpoints/download_ckpts.sh
+++ checkpoints/download_ckpts.sh
@@ -17,24 +17,24 @@ else
 fi
 
 # Define the URLs for SAM 2 checkpoints
-# SAM2_BASE_URL="https://dl.fbaipublicfiles.com/segment_anything_2/072824"
-# sam2_hiera_t_url="${SAM2_BASE_URL}/sam2_hiera_tiny.pt"
-# sam2_hiera_s_url="${SAM2_BASE_URL}/sam2_hiera_small.pt"
-# sam2_hiera_b_plus_url="${SAM2_BASE_URL}/sam2_hiera_base_plus.pt"
-# sam2_hiera_l_url="${SAM2_BASE_URL}/sam2_hiera_large.pt"
+ SAM2_BASE_URL="https://dl.fbaipublicfiles.com/segment_anything_2/072824"
+ sam2_hiera_t_url="${SAM2_BASE_URL}/sam2_hiera_tiny.pt"
+ sam2_hiera_s_url="${SAM2_BASE_URL}/sam2_hiera_small.pt"
+ sam2_hiera_b_plus_url="${SAM2_BASE_URL}/sam2_hiera_base_plus.pt"
+ sam2_hiera_l_url="${SAM2_BASE_URL}/sam2_hiera_large.pt"
 
 # Download each of the four checkpoints using wget
-# echo "Downloading sam2_hiera_tiny.pt checkpoint..."
-# $CMD $sam2_hiera_t_url || { echo "Failed to download checkpoint from $sam2_hiera_t_url"; exit 1; }
+ echo "Downloading sam2_hiera_tiny.pt checkpoint..."
+ $CMD $sam2_hiera_t_url || { echo "Failed to download checkpoint from $sam2_hiera_t_url"; exit 1; }
 
-# echo "Downloading sam2_hiera_small.pt checkpoint..."
-# $CMD $sam2_hiera_s_url || { echo "Failed to download checkpoint from $sam2_hiera_s_url"; exit 1; }
+ echo "Downloading sam2_hiera_small.pt checkpoint..."
+ $CMD $sam2_hiera_s_url || { echo "Failed to download checkpoint from $sam2_hiera_s_url"; exit 1; }
 
-# echo "Downloading sam2_hiera_base_plus.pt checkpoint..."
-# $CMD $sam2_hiera_b_plus_url || { echo "Failed to download checkpoint from $sam2_hiera_b_plus_url"; exit 1; }
+ echo "Downloading sam2_hiera_base_plus.pt checkpoint..."
+ $CMD $sam2_hiera_b_plus_url || { echo "Failed to download checkpoint from $sam2_hiera_b_plus_url"; exit 1; }
 
-# echo "Downloading sam2_hiera_large.pt checkpoint..."
-# $CMD $sam2_hiera_l_url || { echo "Failed to download checkpoint from $sam2_hiera_l_url"; exit 1; }
+ echo "Downloading sam2_hiera_large.pt checkpoint..."
+ $CMD $sam2_hiera_l_url || { echo "Failed to download checkpoint from $sam2_hiera_l_url"; exit 1; }
 
 # Define the URLs for SAM 2.1 checkpoints
 SAM2p1_BASE_URL="https://dl.fbaipublicfiles.com/segment_anything_2/092824"
